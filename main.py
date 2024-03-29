81e0d8a44e98925da1cbf420963b33c1fa102aa7
antho <anthonyjrchen@gmail.com>
2024-03-29 03:51:32 -0700

    Added profile info to vertical menu

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory=r"C:\Users\antho\Desktop\JML Event Handler\static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
+def landing_page(request: Request):
+    return templates.TemplateResponse("landing_page.html", {
+        "request": request,
+    })
+
+@app.get("/home")
 def home(request: Request):
     return templates.TemplateResponse("home.html", {
         "request": request,
@@ -13,21 +21,21 @@ def home(request: Request):
     })
 
 @app.get("/remove")
-def home(request: Request):
+def remove(request: Request):
     return templates.TemplateResponse("remove.html", {
         "request": request,
         "page_location":"remove"
     })
 
 @app.get("/calendars")
-def home(request: Request):
+def calendars(request: Request):
     return templates.TemplateResponse("calendars.html", {
         "request": request,
         "page_location":"calendars"
     })
 
 @app.get("/calculator")
-def home(request: Request):
+def calculators(request: Request):
     return templates.TemplateResponse("calculator.html", {
         "request": request,
         "page_location":"calculator"
