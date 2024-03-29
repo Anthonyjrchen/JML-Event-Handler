ea3fe6e85c5008496667e782eec6e93ef09fa376
antho <anthonyjrchen@gmail.com>
2024-03-29 02:29:29 -0700

    Created home, remove, calendars, calculator skeleton pages with a simple routing system.

--- main.py
+++ main.py
@@ -6,7 +6,29 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html", {
-        "request": request
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "page_location":"home"
+    })
+
+@app.get("/remove")
+def home(request: Request):
+    return templates.TemplateResponse("remove.html", {
+        "request": request,
+        "page_location":"remove"
+    })
+
+@app.get("/calendars")
+def home(request: Request):
+    return templates.TemplateResponse("calendars.html", {
+        "request": request,
+        "page_location":"calendars"
+    })
+
+@app.get("/calculator")
+def home(request: Request):
+    return templates.TemplateResponse("calculator.html", {
+        "request": request,
+        "page_location":"calculator"
     })
