99279d18b8ea31d4e85204fe2d8b67e5e5191936
antho <anthonyjrchen@gmail.com>
2024-05-08 00:34:16 -0700

    Added login and signup user functionality.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi import FastAPI, Request, Depends, HTTPException, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from database import SessionLocal, engine
@@ -6,9 +6,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 import models
 from models import User
+from datetime import timedelta
 import cryptography
 import pandas as pd
 import win32com.client as client
@@ -53,12 +56,15 @@ USER SIGNUP
 class UserRequest(BaseModel):
     username: str
     password: str
+    occupation: int
+    first_name: str
+    last_name: str
 
 @app.post("/signup")
 async def create_user(request: UserRequest, db : Session = Depends(get_db)):
     try:
         hashed_password = cryptography.get_password_hash(request.password)
-        user = models.User(username=request.username, hashed_password=hashed_password)
+        user = models.User(username=request.username, hashed_password=hashed_password, occupation=request.occupation, first_name = request.first_name, last_name = request.last_name)
         db.add(user)
         db.commit()
         return {"True"}
@@ -75,9 +81,10 @@ def landing_page(request: Request):
 '''
 USER LOGIN
 '''
+
 def get_user(username: str, db):
-    users = db.query(User).filter(User.username==username).first()
-    return users
+    user = db.query(User).filter(User.username==username).first()
+    return user
 
 def authenticate_user(username: str, password: str, db):
     user = get_user(username, db)
@@ -87,12 +94,38 @@ def authenticate_user(username: str, password: str, db):
         return False
     return user
 
+
+@app.post("/login")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), Authorize: AuthJWT = Depends(), db : Session = Depends(get_db)):    
+    print("login function called")
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=cryptography.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = Authorize.create_access_token(subject=user.username, algorithm=cryptography.ALGORITHM ,expires_time=access_token_expires)
+    refresh_token = Authorize.create_refresh_token(subject=user.username, algorithm=cryptography.ALGORITHM ,expires_time=access_token_expires)
+    Authorize.set_access_cookies(access_token)
+    Authorize.set_refresh_cookies(refresh_token)
+    return {"msg":"Login Successful"}
+
+'''
+LOG OUT
+'''
+@app.delete('/logout')
+def logout(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    Authorize.unset_jwt_cookies()
+    return {"msg": "Logout Successful"}
+
+
 '''
 SUPPORT TICKET ROUTE
 '''
 @app.post("/ticket/submit")
-def support(request: Request):
-    
+def support(request: Request, Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
     return request.headers["subject"] + " : " + request.headers["body"]
 
 
@@ -106,7 +139,8 @@ def landing_page(request: Request):
     })
 
 @app.get("/home")
-async def home(request: Request):
+async def home(request: Request, Authorize: AuthJWT = Depends(), db : Session = Depends(get_db)):
+    Authorize.jwt_required()
     calendars = []
     temp = []
     for idx, a in enumerate(namespace.getDefaultFolder(9).Folders):
@@ -118,24 +152,28 @@ async def home(request: Request):
         "request": request,
         "page_location":"home",
         "calendars":calendars,
+        "fn":get_user(Authorize.get_jwt_subject(), db).first_name,
     })
 
 @app.get("/calendars")
-def calendars(request: Request):
+def calendars(request: Request, Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
     return templates.TemplateResponse("calendars.html", {
         "request": request,
         "page_location":"calendars"
     })
 
 @app.get("/settings")
-def remove(request: Request):
+def remove(request: Request, Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
     return templates.TemplateResponse("settings.html", {
         "request": request,
         "page_location":"settings"
     })
 
 @app.get("/support")
-def support(request: Request):
+def support(request: Request, Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
     return templates.TemplateResponse("support.html", {
         "request": request,
         "page_location":"support"
